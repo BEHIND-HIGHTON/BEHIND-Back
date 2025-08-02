01bf562c8b242f3960d035d3a5f42f3e17879114
Stephan Kim <hhkimstar1226@gmail.com>
2025-08-03 06:11:22 +0900

    변경: auth 모듈에서 TokenPayload를 TokenData로 수정하여 스키마 초기화 업데이트

--- app/schemas/__init__.py
+++ app/schemas/__init__.py
@@ -1,5 +1,5 @@
 # app.schemas 패키지 초기화
-from .auth import Token, TokenPayload
+from .auth import Token, TokenData
 from .item import Item, ItemCreate, ItemInDB
 from .user import User, UserCreate, UserInDB, UserUpdate
 from .partner import Partner, PartnerCreate, PartnerUpdate, PartnerList
